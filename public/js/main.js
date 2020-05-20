247670fdaaddffa53108c6ea935ab116888ed1b0
Antony Chiramel <AntonyChiramel@teamblr.com>
2020-05-20 20:36:03 +0530

    Fixed the location url bug

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
-if(document.location.href === 'http://localhost:3000/') {
+if(document.location.href === 'http://localhost:3000/' || document.location.href === 'https://helloantony.herokuapp.com/') {
     document.querySelector('.home-link').style.color = "rgb(255,190,0)";
-} else if(document.location.href === 'http://localhost:3000/about') {
+} else if(document.location.href === 'http://localhost:3000/about' || document.location.href === 'https://helloantony.herokuapp.com/about') {
     document.querySelector('.about-link').style.color = "rgb(255,190,0)";
-} else if(document.location.href === 'http://localhost:3000/projects') {
+} else if(document.location.href === 'http://localhost:3000/projects' || document.location.href === 'https://helloantony.herokuapp.com/projects') {
     document.querySelector('.projects-link').style.color = 'rgb(255,190,0)';
 } else {
     document.querySelector('.contact-link').style.color = 'rgb(255,190,0)';
